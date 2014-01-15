831873ba96a99427e6586a086bfa31f2edb83da8
Brad Marshall <brad.marshall@canonical.com>
2014-01-15 14:03:23 +1000

    PEP8 fixes for hooks.py

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -42,27 +42,28 @@ charm_dir = os.path.dirname(hook_dir)
 
 """
 
+
 def setup_udldap():
     configure_sources(True, 'apt-repo-spec', 'apt-repo-keys')
     apt_install('userdir-ldap')
     if not os.path.exists('/root/.ssh'):
         os.makedirs('/root/.ssh')
     shutil.copyfile('%s/files/nsswitch.conf' % charm_dir,
-                        '/etc/nsswitch.conf')
+                    '/etc/nsswitch.conf')
     # adelie in hosts
     userdb = "91.189.90.139   userdb.internal"
     with open('/etc/hosts', 'a+') as f:
         for line in f:
             if userdb in line:
                 break
-        else: # not found
+        else:  # not found
             print >> f, userdb
     # If we don't assert these symlinks in /etc, ud-replicate
     # will write to them for us and trip up the local changes check.
     if not os.path.islink('/etc/ssh/ssh-rsa-shadow'):
-        os.symlink('/var/lib/misc/ssh-rsa-shadow','/etc/ssh/ssh-rsa-shadow')
+        os.symlink('/var/lib/misc/ssh-rsa-shadow', '/etc/ssh/ssh-rsa-shadow')
     if not os.path.islink('/etc/ssh/ssh_known_hosts'):
-        os.symlink('/var/lib/misc/ssh_known_hosts','/etc/ssh/ssh_known_hosts')
+        os.symlink('/var/lib/misc/ssh_known_hosts', '/etc/ssh/ssh_known_hosts')
     # The first run of ud-replicate requires that
     # userdb.internal's host key be trusted.
     os.system('/usr/bin/ssh-keyscan -t rsa userdb.internal \
@@ -74,7 +75,7 @@ def setup_udldap():
         os.remove('/root/.ssh/id_rsa')
         os.remove('/root/.ssh/id_rsa.pub')
     subprocess.check_call(['/usr/bin/ssh-keygen', '-q', '-t', 'rsa',
-        '-b', '2048', '-N', '', '-f', '/root/.ssh/id_rsa'])
+                           '-b', '2048', '-N', '', '-f', '/root/.ssh/id_rsa'])
     # Force initial run
     subprocess.check_call(['/usr/bin/ud-replicate'])
     # Setup cron with a little variation
@@ -88,7 +89,8 @@ def setup_udldap():
     # All done
     subprocess.check_call(['bzr', 'add', '/etc'])
     subprocess.check_call(['bzr', 'ci', '/etc', '-m',
-        '"', 'setup', 'ud-ldap', '"'])
+                           '"', 'setup', 'ud-ldap', '"'])
+
 
 @hooks.hook("install")
 def install():
