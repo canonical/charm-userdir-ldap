ac6d78caeaf79134fbee73e5c8dbd6c88bf8cbf7
peppepetra <giuseppe.petralia@canonical.com>
2022-09-28 12:14:25 +0200

    Verify proxy settings in pip_install
    When evaluating proxy settings for pip_install,
    use http_proxy only if it is defined and
    pypi.org is not in no_proxy

--- hooks/utils.py
+++ hooks/utils.py
@@ -33,7 +33,25 @@ try:
 except ImportError:
     apt_update(fatal=True)
     apt_install("python3-pip", fatal=True)
-    packages.pip_install("python-hosts", proxy=env_proxy_settings(["http"]).get("http_proxy"))
+    
+    # Get proxy settings
+    proxy_settings = env_proxy_settings(["http", "no_proxy"])
+    http_proxy = proxy_settings.get("http_proxy")
+    no_proxy = proxy_settings.get("no_proxy")
+    
+    # Only pass http_proxy to pip_install if it is defined
+    # and no_proxy does not include 'pypi.org'
+    proxy = None
+    if http_proxy and not no_proxy:
+        proxy = http_proxy
+    if http_proxy and no_proxy and "pypi.org" not in no_proxy:
+        proxy = http_proxy
+    
+    if proxy:
+        packages.pip_install("python-hosts", proxy=proxy)
+    else:
+        packages.pip_install("python-hosts")
+
     from python_hosts.hosts import Hosts, HostsEntry
 
 
